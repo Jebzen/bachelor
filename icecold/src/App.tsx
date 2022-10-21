793d8d197252f57b49d926be55a273f54273f495
Jeppe Bentzen <55578352+Jebzen@users.noreply.github.com>
2022-10-21 10:42:02 +0200

    no svg

--- icecold/src/App.tsx
+++ icecold/src/App.tsx
@@ -1,9 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import {
   Route,
-  Link,
   Routes
 } from "react-router-dom";
 import { Home } from './components/Home';
