b29542c9da4c6838058ed41862db6e627ec984f5
Jebzen <jeb.bentzen@gmail.com>
2022-11-14 13:29:16 +0100

    New next application

--- icecold/src/components/NotFound.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function NotFound(){
-    return (
-        <main className="notfound">
-            <h1>404 not found</h1>
-        </main>
-    )
-}
--- icecold/src/components/Section.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export function Section(props : any){
-    return <section>{props.children}</section>
-}
