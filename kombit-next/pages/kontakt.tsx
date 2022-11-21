0ab2de25c6de387f97da893b67225a782a67588d
Jebzen <jeb.bentzen@gmail.com>
2022-11-21 14:15:07 +0100

    Data for kontaktformular

--- kombit-next/pages/kontakt.tsx
+++ kombit-next/pages/kontakt.tsx
@@ -1,6 +1,48 @@
 import { client } from "../components/contenful/main";
 import { IndexLayout } from "../layout";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 export default function Kontakt() {
-	return <>Bwah</>;
+	const form: any = useRef();
+
+	const sendEmail = (e: any) => {
+		e.preventDefault();
+
+		emailjs
+			.sendForm(
+				"service_96jb83y",
+				"template_o8fqska",
+				form.current,
+				"JMbkZmQe-OiIyjo_l"
+			)
+			.then(
+				(result) => {
+					console.log(result.text);
+				},
+				(error) => {
+					console.log(error.text);
+				}
+			);
+	};
+	return (
+		<section className="container h-100">
+			<h1>Kontaktformular</h1>
+			<form ref={form} onSubmit={sendEmail}>
+				<div className="mb-3">
+					<label className="form-label">Navn</label>
+					<input type="text" name="name" className="form-control" />
+				</div>
+				<div className="mb-3">
+					<label className="form-label">Email</label>
+					<input type="email" name="email" className="form-control" />
+				</div>
+				<div className="mb-3">
+					<label className="form-label">Besked</label>
+					<textarea name="message" className="form-control" />
+				</div>
+				<input type="submit" value="Send" className="btn btn-primary" />
+			</form>
+		</section>
+	);
 }
