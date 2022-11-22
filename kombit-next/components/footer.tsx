74c0e95d83b3ebe0d1a94699c0e6eb60a291114d
Jebzen <jeb.bentzen@gmail.com>
2022-11-22 11:47:48 +0100

    Footer 2

--- kombit-next/components/footer.tsx
+++ kombit-next/components/footer.tsx
@@ -29,18 +29,47 @@ export default function Footer() {
 
 export default function Footer({ infoPages }: any) {
 	return (
-		<footer className="p-2 d-flex flex-column">
-			<h3>KOMBIT A/S</h3>
-			<div className="footerbox">
-				{infoPages &&
-					infoPages.length > 0 &&
-					infoPages.map((page: any, i: number) => {
-						return (
-							<a key={i} href={"/indhold/" + page.fields.slug}>
-								{page.fields.title}
-							</a>
-						);
-					})}
+		<footer>
+			<div className="p-2 d-flex flex-column">
+				<h3>KOMBIT A/S</h3>
+				<div className="footerbox">
+					{infoPages &&
+						infoPages.length > 0 &&
+						infoPages.map((page: any, i: number) => {
+							return (
+								<a
+									key={i}
+									href={"/indhold/" + page.fields.slug}
+								>
+									{page.fields.title}
+								</a>
+							);
+						})}
+				</div>
+			</div>
+			<div className="p-2 footerbox-2">
+				<div>
+					<p>KOMBIT A/S, Halfdansgade 8, 2300 København S</p>
+					<p>CVR-nr.: 19435075 | EAN-nr. 5790001969370</p>
+					<p>3334 9400 | kombit@kombit.dk</p>
+				</div>
+				<div className="d-flex justify-content-end">
+					<p>Find os på:</p>
+					<p>
+						<a
+							href="http://www.linkedin.com/company/kombit-a-s"
+							className="fs-4 ms-2"
+						>
+							<i className="bi bi-linkedin"></i>
+						</a>
+						<a
+							href="https://twitter.com/kombitdk"
+							className="fs-4 mx-2"
+						>
+							<i className="bi bi-twitter"></i>
+						</a>
+					</p>
+				</div>
 			</div>
 		</footer>
 	);
