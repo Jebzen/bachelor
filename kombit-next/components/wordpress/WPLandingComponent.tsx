b36190f52dc24b6ad94198ac0e5be7d5817da993
signekp <31508673+signekp@users.noreply.github.com>
2022-12-07 11:41:29 +0100

    style

--- kombit-next/components/wordpress/WPLandingComponent.tsx
+++ kombit-next/components/wordpress/WPLandingComponent.tsx
@@ -69,14 +69,14 @@ export default function WPLandingComponent({ content }: prop) {
 					</>
 				)}
 			</div>
-			<div className="row">
-				<div className="col-9">
+			<div className="row landingInfoContainer">
+				<div className="col-9 projektLanding">
 					<span dangerouslySetInnerHTML={{ __html: content.content }} />
 				</div>
 
 				{kontakt_person && kontakt_person.data.mediaItem && (
 					<>
-						<div className="col-3">
+						<div className="col-3 projektLanding">
 							<div className="d-flex justify-end flex-column">
 								<span
 									className="text-end fs-4"
@@ -110,64 +110,3 @@ export default function WPLandingComponent({ content }: prop) {
 }
 
 
-{/* <>
-		
-		<section className="container news">
-			<div className="p-2 my-2">
-				<h4 className={styles.HeaderFour}>PROJEKTER: TEKNIK, MILJØ & BORGERSERVICE</h4>
-				<div className="row land">
-					{content.fields.sider.map((side: any, i: number) => {
-						return (
-							<div key={i} data-id={i} className="col-2 landing" onClick={changeSlide}>
-								<img
-									src={side.fields.featuredImage.fields.file.url}
-									className={styles.landingImg}
-									alt={side.fields.featuredImage.fields.file.title}
-								/>
-								<div className={styles.flexDiv}><p className="text-center"><b>{side.fields.title}</b></p>
-								<a className={styles.readMore} href={"/projekt/" + side.fields.slug}>
-         Læs mere
-        </a></div>
-								
-							</div>
-						);
-					})}
-					{slide !== null && (
-						<div className="mt-2">
-							<h3 className="fw-bold">{slide.fields.title}</h3>
-							<div>{slide.fields.abstrakt}</div>
-							<a
-								href={"projekter/" + slide.fields.slug}
-								className="d-flex justify-content-end"
-							>
-								<p className="text-end">Læs mere</p>
-							</a>
-						</div>
-					)}
-				</div>
-			</div>
-			<div className="row landingInfoContainer">
-				<div className="col-9 projektLanding">
-					<h3>Mission</h3>
-					{documentToReactComponents(content.fields.mission)}
-				</div>
-				<div className="col-3 projektLanding">
-					<div className="d-flex justify-end flex-column">
-						<h4 className="text-end">Forretningschef</h4>
-						<img
-							className="img-fluid"
-							src={content.fields.forretningschef.fields.file.url}
-							alt={content.fields.forretningschef.fields.title}
-						/>
-						<p>{content.fields.forretningschef.fields.title}</p>
-						<p className="respect-line">
-							{content.fields.forretningschef.fields.description}
-						</p>
-					</div>
-				</div>
-				<div className="col-12">
-					<CFFeed />
-				</div>
-			</div>
-		</section>
-		</> */}
