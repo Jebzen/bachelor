08e278aef01110a3ac5fde79315ed959de1c0178
signekp <31508673+signekp@users.noreply.github.com>
2022-12-07 20:06:31 +0100

    style

--- kombit-next/components/contenful/CFInfoComponent.tsx
+++ kombit-next/components/contenful/CFInfoComponent.tsx
@@ -4,6 +4,8 @@ import SoMeFeed from "../general/SoMeFeed";
 import ShareButtons from "../general/ShareButtons";
 import { CFEntryIndhold } from "../../interfaces/CFentry";
 import PageHero from "../general/PageHero";
+import styles from "../../styles/Projekt.module.css";
+
 
 interface prop {
 	content: CFEntryIndhold;
@@ -16,7 +18,7 @@ export default function CFInfoComponent({ content }: prop) {
 		renderNode: {
 			"embedded-asset-block": (node: any, children: any) => {
 				return (
-					<div className="text-center mansory">
+					<div className="text-center mansory featuredImage">
 						<img src={node.data.target.fields.file.url} width="100%" className=" images" />
 					</div>
 				);
@@ -52,21 +54,67 @@ export default function CFInfoComponent({ content }: prop) {
 				)}</div>
 							
 
-				<div className="beskrivelse-news">{documentToReactComponents(content.fields.beskrivelse, renderOption)}</div>
+				<div className="beskrivelse-news featuredImage-img">{documentToReactComponents(content.fields.beskrivelse, renderOption)}</div>
 				</div>
 
 						</section>
+						{content.fields.sectionTo && (			<div className="sectionOne">
+			
+			<div className="table-col">{documentToReactComponents(content.fields.sectionTo, renderOption)}</div>
 
-				<div className="galleri">{documentToReactComponents(content.fields.billedeGalleri, renderOption)}</div>
-				<section className="container news">
+</div>)}
+	{ content.fields.sectionTre &&(<div className={ `text-center`}>{documentToReactComponents(content.fields.sectionTre, renderOption)}
+								
 
-								{documentToReactComponents(content.fields.sectionTo, renderOption)}
-
+								</div>)}
 
 				
+				{/* {documentToReactComponents(content.fields.billedeGalleri, renderOption)} */}
+
+				<section className="container news con2">
+				{content.fields.sectionFire && (<div className="secTwo">
+<div className="colOne">{documentToReactComponents(content.fields.sectionFire)}</div>
+{documentToReactComponents(content.fields.pic1, renderOption)}
+
+
+
+</div>)}
+
+
+{ content.fields.pic2 &&(<div className="secTree">
+{documentToReactComponents(content.fields.pic2, renderOption)}
+
+<div className="colOne">{documentToReactComponents(content.fields.sectionFem)}</div>
+
+
+
+</div>)}
+
+
+
+<div className={ `text-center`}>
+								
+
+								</div>
+						
+
+		
+
+			
 				
-				<SoMeFeed />
 				</section>
+
+				{content.fields.sectionSeks &&(	<div className={styles.specialMargin + ` ` + `sectionOne`}>
+			
+			<div className="table-col">{documentToReactComponents(content.fields.sectionSeks)}</div>
+
+</div>)}
+
+			
+
+<div className={styles.container}>
+<SoMeFeed />      </div>
+
 		</>
 	);
 }
