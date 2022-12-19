4f923335f5c43abf42d9f227afe97ed4fe70ca4a
Jebzen <jeb.bentzen@gmail.com>
2022-12-19 10:53:25 +0100

    Update CFReferencer.tsx

--- kombit-next/components/contenful/CFReferencer.tsx
+++ kombit-next/components/contenful/CFReferencer.tsx
@@ -1,59 +1,85 @@
 export default function CFReferencer({ content }: any) {
 	//console.log(content.sys.contentType.sys.id);
 
+	//Content er arrayet af reference
+	//Content bliver mappet til "reference" hver
+	//Reference har en "reference.sys.contentType.sys.id" property som er hvilken content model det er
+	//Det er her hvor media,banner,featured-image værdierne er anderledes og har brug for hver sit punkt
+	//Linje 15 er hvor jeg logger hver reference ud, hvor du kan se hvad hvert reference indeholder
+
 	return (
 		<div className="news-related">
-			{content.map((reference: any) => (
-				<>
-					{reference.sys.contentType.sys.id == "nyheder" && (
-						<div className="related">
-							<a
-								href={
-									"/" +
-									reference.sys.contentType.sys.id +
-									"/" +
-									reference.fields.slug
-								}
-							>
-								<img
-									src={reference.fields.banner.fields.file.url}
-									alt={reference.fields.banner.fields.title}
-									className="newsImg-related"
-								/>
-								<p className="small">
-									Udgivet d. {reference.sys.createdAt.substring(0, 10)}
-								</p>
-								<h3>{reference.fields.title}</h3>
-								<p>{reference.fields.abstrakt}</p>
-							</a>
-						</div>
-					)}
+			{content.map((reference: any) => {
+				//Hvad indeholder referencen
+				console.group("Reference");
+				console.log(reference); //Indhold
+				console.log(reference.sys?.contentType?.sys?.id); //Content model;
+				console.groupEnd();
 
-					{reference.sys.contentType.sys.id == "landingpage" && (
-						<div className="related">
-							<a
-								href={
-									"/" +
-									reference.sys.contentType.sys.id +
-									"/" +
-									reference.fields.slug
-								}
-							>
-								<img
-									src={reference.fields.media.fields.file.url}
-									alt={reference.fields.media.fields.title}
-									className="newsImg-related"
-								/>
-								<p className="small">
-									Udgivet d. {reference.sys.createdAt.substring(0, 10)}
-								</p>
-								<h3>{reference.fields.title}</h3>
-								<p>{reference.fields.abstrakt}</p>
-							</a>
-						</div>
-					)}
-				</>
-			))}
+				return (
+					<>
+						{reference.sys.contentType.sys.id == "nyheder" && (
+							<div className="related">
+								<a
+									href={
+										"/" +
+										reference.sys.contentType.sys.id +
+										"/" +
+										reference.fields.slug
+									}
+								>
+									<img
+										src={reference.fields.banner.fields.file.url}
+										alt={reference.fields.banner.fields.title}
+										className="newsImg-related"
+									/>
+									<p className="small">
+										Udgivet d. {reference.sys.createdAt.substring(0, 10)}
+									</p>
+									<h3>{reference.fields.title}</h3>
+									<p>{reference.fields.abstrakt}</p>
+								</a>
+							</div>
+						)}
+
+						{reference.sys.contentType.sys.id == "landingpage" && (
+							<div className="related">
+								<a
+									href={
+										"/" +
+										reference.sys.contentType.sys.id +
+										"/" +
+										reference.fields.slug
+									}
+								>
+									<img
+										src={reference.fields.media.fields.file.url}
+										alt={reference.fields.media.fields.title}
+										className="newsImg-related"
+									/>
+									<p className="small">
+										Udgivet d. {reference.sys.createdAt.substring(0, 10)}
+									</p>
+									<h3>{reference.fields.title}</h3>
+									<p>{reference.fields.abstrakt}</p>
+								</a>
+							</div>
+						)}
+
+						{reference.sys.contentType.sys.id == "infoside" && (
+							<div className="related"></div>
+						)}
+
+						{reference.sys.contentType.sys.id == "kalender" && (
+							<div className="related"></div>
+						)}
+
+						{reference.sys.contentType.sys.id == "projekt" && (
+							<div className="related"></div>
+						)}
+					</>
+				);
+			})}
 		</div>
 	);
 }
